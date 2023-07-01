54d11806a1ed1dc19b8c4d69706250a57b477825
yanfalcao <yanfalcao10@gmail.com>
2023-07-01 01:35:24 -0300

    Create login screen using Compose

---
+++ app/src/main/java/com/example/receptia/LoginActivity.kt
@@ -0,0 +1,149 @@
+package com.example.receptia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.receptia.ui.theme.ReceptIaTheme
+import com.example.receptia.ui.theme.loginBody
+import com.example.receptia.ui.theme.titleBoldLarge
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Body()
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+)
+@Composable
+fun Body() {
+    ReceptIaTheme {
+        Background()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            Title()
+            Spacer(modifier = Modifier.height(20.dp))
+            Description()
+            Spacer(modifier = Modifier.height(50.dp))
+            GoogleLoginButton()
+            Spacer(modifier = Modifier.height(50.dp))
+        }
+    }
+}
+
+@Composable
+fun Background() {
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val gradientBrush = Brush.verticalGradient(
+            colors = listOf(Color.Transparent, Color.Black),
+            startY = 0f,
+            endY = constraints.maxHeight.toFloat(),
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.background_login),
+            contentDescription = stringResource(id = R.string.bg_login_description),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(),
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(gradientBrush),
+        )
+    }
+}
+
+@Composable
+fun Title() {
+    Text(
+        text = stringResource(id = R.string.title_login),
+        color = Color.White,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.titleBoldLarge,
+        modifier = Modifier.width(300.dp),
+    )
+}
+
+@Composable
+fun Description() {
+    Text(
+        text = stringResource(id = R.string.description_login),
+        color = Color.White,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier
+            .width(300.dp),
+    )
+}
+
+@Composable
+fun GoogleLoginButton() {
+    Button(
+        onClick = { /*TODO*/ },
+        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+        modifier = Modifier
+            .height(55.dp)
+            .fillMaxWidth()
+            .padding(start = 25.dp, end = 25.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_google),
+                contentDescription = stringResource(id = R.string.google_icon_description),
+                modifier = Modifier
+                    .height(32.dp)
+                    .width(32.dp)
+                    .align(Alignment.CenterVertically),
+            )
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Text(
+                text = stringResource(id = R.string.google_login),
+                color = Color.Black,
+                style = MaterialTheme.typography.loginBody,
+                modifier = Modifier.align(Alignment.CenterVertically),
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/receptia/ui/theme/Type.kt
+++ app/src/main/java/com/example/receptia/ui/theme/Type.kt
@@ -1,34 +1,50 @@
 package com.example.receptia.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.receptia.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
+val RalewayFont = FontFamily(
+    Font(R.font.raleway, FontWeight.Normal),
+    Font(R.font.raleway_bold, FontWeight.Bold),
+    Font(R.font.raleway_semibold, FontWeight.SemiBold),
+    Font(R.font.raleway_extrabold, FontWeight.ExtraBold),
+)
+
+val Typography.titleBoldLarge: TextStyle
+    @Composable
+    get() {
+        return TextStyle(
+            fontFamily = RalewayFont,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            lineHeight = 36.sp,
+            letterSpacing = 0.4.sp,
+        )
+    }
+
+val Typography.loginBody: TextStyle
+    @Composable
+    get() {
+        return TextStyle(
+            fontFamily = RalewayFont,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp,
+            letterSpacing = 0.2.sp,
+        )
+    }
+
+val ReceptIaTypography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RalewayFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 20.sp,
+        letterSpacing = 0.2.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
