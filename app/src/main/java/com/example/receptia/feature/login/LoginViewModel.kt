89830de3b3635af68cc5f748c94ceba0c6edd971
yanfalcao <yanfalcao10@gmail.com>
2023-07-14 13:49:02 -0300

    Adjust viewModel instance

--- app/src/main/java/com/example/receptia/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/example/receptia/feature/login/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.receptia.feature.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -9,11 +11,13 @@ import kotlinx.coroutines.launch
 
 class LoginViewModel : ViewModel() {
     private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Started)
-    val loginUiState: StateFlow<LoginUiState> = _loginUiState.asStateFlow()
+    val loginUiState get() = _loginUiState
 
     // TODO: Implement login logic
     fun loginGoogle() {
         viewModelScope.launch {
+            _loginUiState.value = LoginUiState.Loading
+            delay(2000)
             _loginUiState.value = LoginUiState.Success(data = true)
         }
     }
