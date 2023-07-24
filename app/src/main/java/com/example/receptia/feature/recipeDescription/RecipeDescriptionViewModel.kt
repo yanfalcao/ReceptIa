2dd89878e96e2973d8c599624a6c96dc83173231
yanfalcao <yanfalcao10@gmail.com>
2023-07-24 17:28:37 -0300

    Implements toogleButton on RecipeDescriptionScreen

--- app/src/main/java/com/example/receptia/feature/recipeDescription/RecipeDescriptionViewModel.kt
+++ app/src/main/java/com/example/receptia/feature/recipeDescription/RecipeDescriptionViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.receptia.feature.recipeDescription
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.receptia.feature.recipeDescription.state.ToogleRecipeState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class RecipeDescriptionViewModel : ViewModel() {
+    private val _toogleRecipeState = MutableStateFlow<ToogleRecipeState>(ToogleRecipeState.DetailsSelected)
+    val toogleRecipeState get() = _toogleRecipeState
+
+    fun selectRecipeToogle() {
+        viewModelScope.launch {
+            _toogleRecipeState.value = when (_toogleRecipeState.value) {
+                ToogleRecipeState.DetailsSelected -> ToogleRecipeState.RecipeSelected
+                ToogleRecipeState.RecipeSelected -> ToogleRecipeState.DetailsSelected
+            }
+        }
+    }
 }
