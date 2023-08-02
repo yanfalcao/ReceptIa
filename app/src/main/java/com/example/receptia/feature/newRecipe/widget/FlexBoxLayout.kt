000f9b61a5fc2fb4d28310d62b0df4a7abf16017
yanfalcao <yanfalcao10@gmail.com>
2023-08-02 00:24:02 -0300

    Implement FlexBoxLayout;

---
+++ app/src/main/java/com/example/receptia/feature/newRecipe/widget/FlexBoxLayout.kt
@@ -0,0 +1,55 @@
+package com.example.receptia.feature.newRecipe.widget
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.receptia.ui.theme.LightGreen
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun FlexBoxLayout(ingredientList: List<String>) {
+    FlowRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        for (ingredient in ingredientList) {
+            Row(
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .background(
+                        color = LightGreen,
+                        shape = RoundedCornerShape(20.dp),
+                    )
+                    .padding(vertical = 6.dp, horizontal = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = ingredient,
+                    style = MaterialTheme.typography.labelSmall,
+                    textAlign = TextAlign.Center,
+                )
+
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    modifier = Modifier.size(14.dp),
+                    contentDescription = null,
+                )
+            }
+        }
+    }
+}
