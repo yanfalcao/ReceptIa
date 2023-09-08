62018e3a54d4e6f062f7e9bcae85656cf9360ad2
yanfalcao <yanfalcao10@gmail.com>
2023-09-08 03:11:30 -0300

    Feed HomeScreen with last recipes when start and restart an activity

--- app/src/main/java/com/example/receptia/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/receptia/feature/home/HomeViewModel.kt
@@ -4,20 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.receptia.feature.home.state.RecipeFeedUiState
 import com.example.receptia.persistence.Recipe
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class HomeViewModel : ViewModel() {
-    val feedState: StateFlow<RecipeFeedUiState> =
-        flow<RecipeFeedUiState> {
+    private val _lastRecipesUiState = MutableStateFlow<RecipeFeedUiState>(RecipeFeedUiState.Loading)
+    val lastRecipesUiState: StateFlow<RecipeFeedUiState> = _lastRecipesUiState
+
+    fun updateLastRecipes() {
+        viewModelScope.launch {
+            _lastRecipesUiState.value = RecipeFeedUiState.Loading
             val recipeList = Recipe.find(limit = 10)
 
-            emit(RecipeFeedUiState.Success(recipes = recipeList))
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = RecipeFeedUiState.Loading,
-        )
+            _lastRecipesUiState.value = RecipeFeedUiState.Success(recipes = recipeList)
+        }
+    }
 }
