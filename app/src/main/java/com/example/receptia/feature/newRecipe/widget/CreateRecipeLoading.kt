3ec2e6ad6a3c8c4c745e693f2b6b61ba81ec53d0
yanfalcao <yanfalcao10@gmail.com>
2023-09-09 01:09:33 -0300

    Add preview in NewRecipeScreen

--- app/src/main/java/com/example/receptia/feature/newRecipe/widget/CreateRecipeLoading.kt
+++ app/src/main/java/com/example/receptia/feature/newRecipe/widget/CreateRecipeLoading.kt
@@ -1,13 +1,10 @@
 package com.example.receptia.feature.newRecipe.widget
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,9 +17,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
@@ -32,6 +29,7 @@ import com.airbnb.lottie.compose.rememberLottieComposition
 import com.example.receptia.R
 import com.example.receptia.ui.theme.BlackTransparent
 
+@Preview(showBackground = true)
 @Composable
 fun CreateRecipeLoading() {
     val anim = rememberLottieAnimatable()
