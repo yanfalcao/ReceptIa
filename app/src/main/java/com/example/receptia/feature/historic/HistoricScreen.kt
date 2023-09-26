68e97b454c0a13a5a7d57f9a956ba37fb3e03b15
yanfalcao <yanfalcao10@gmail.com>
2023-09-26 16:09:57 -0300

    Change recipe historic screen TopBar to open the drawer

--- app/src/main/java/com/example/receptia/feature/historic/HistoricScreen.kt
+++ app/src/main/java/com/example/receptia/feature/historic/HistoricScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -21,6 +23,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.receptia.R
 import com.example.receptia.feature.historic.preview.PreviewParameterData
 import com.example.receptia.feature.historic.state.RecipeHistoricUiState
@@ -30,6 +33,7 @@ import com.example.receptia.feature.historic.widget.SearchBar
 import com.example.receptia.feature.historic.widget.Tag
 import com.example.receptia.feature.historic.widget.LoadingRecipeList
 import com.example.receptia.ui.widget.EmptyStateWidget
+import com.example.receptia.view.widget.NavigationDrawerWidget
 import com.example.receptia.view.widget.TopBarWidget
 
 @Composable
@@ -41,73 +45,81 @@ internal fun HistoricRoute(
 
     HistoricScreen(
         historicState = historicState,
-        onBackClick = navController::popBackStack,
+        navController = navController
     )
 }
 
 @Composable
 private fun HistoricScreen(
     historicState: RecipeHistoricUiState,
-    onBackClick: () -> Unit = {},
+    navController: NavController,
 ) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
     val filterTags = listOf(
         stringResource(id = R.string.all),
         stringResource(id = R.string.favorites),
     )
 
-    Scaffold(
-        topBar = {
-            TopBarWidget(
-                title = stringResource(id = R.string.historic_title),
-                drawerEnabled = false,
-                onBackClick = onBackClick,
-            )
-        },
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxSize()
-                .padding(start = 25.dp, end = 25.dp, top = 20.dp),
-            verticalArrangement = Arrangement.spacedBy(20.dp),
-        ) {
-            Row {
-                SearchBar(
-                    modifier = Modifier
-                        .weight(1.0f)
-                        .height(40.dp),
+    NavigationDrawerWidget(
+        drawerState = drawerState,
+        navController = navController,
+    ) {
+        Scaffold(
+            topBar = {
+                TopBarWidget(
+                    title = stringResource(id = R.string.historic_title),
+                    drawerState = drawerState,
                 )
-
-                Spacer(modifier = Modifier.width(15.dp))
-
-                FilterButton(modifier = Modifier.size(40.dp))
-            }
-
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
+            },
+        ) { padding ->
+            Column(
+                modifier = Modifier
+                    .padding(padding)
+                    .fillMaxSize()
+                    .padding(start = 25.dp, end = 25.dp, top = 20.dp),
+                verticalArrangement = Arrangement.spacedBy(20.dp),
             ) {
-                for (tag in filterTags) {
-                    Tag(text = tag)
-                }
-            }
+                Row {
+                    SearchBar(
+                        modifier = Modifier
+                            .weight(1.0f)
+                            .height(40.dp),
+                    )
 
-            when (historicState) {
-                RecipeHistoricUiState.Loading -> LoadingRecipeList()
-                is RecipeHistoricUiState.Success -> {
-                    if (historicState.recipes.isNotEmpty()) {
-                        GridList(historicState.recipes)
-                    } else {
-                        Box(
-                            modifier = Modifier.weight(1.0f).fillMaxSize(),
-                            contentAlignment = Alignment.Center,
-                        ) {
-                            EmptyStateWidget()
+                    Spacer(modifier = Modifier.width(15.dp))
+
+                    FilterButton(modifier = Modifier.size(40.dp))
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    for (tag in filterTags) {
+                        Tag(text = tag)
+                    }
+                }
+
+                when (historicState) {
+                    RecipeHistoricUiState.Loading -> LoadingRecipeList()
+                    is RecipeHistoricUiState.Success -> {
+                        if (historicState.recipes.isNotEmpty()) {
+                            GridList(historicState.recipes)
+                        } else {
+                            Box(
+                                modifier = Modifier
+                                    .weight(1.0f)
+                                    .fillMaxSize(),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                EmptyStateWidget()
+                            }
                         }
                     }
                 }
             }
         }
     }
+
 }
 
 @Preview(
@@ -118,6 +130,7 @@ private fun HistoricScreen(
 private fun HistoricScreenPreview() {
     HistoricScreen(
         historicState = RecipeHistoricUiState.Success(PreviewParameterData.recipeList),
+        navController = rememberNavController(),
     )
 }
 
@@ -129,6 +142,7 @@ private fun HistoricScreenPreview() {
 private fun LoadingStatePreview() {
     HistoricScreen(
         historicState = RecipeHistoricUiState.Loading,
+        navController = rememberNavController(),
     )
 }
 
@@ -140,5 +154,6 @@ private fun LoadingStatePreview() {
 private fun EmptyStatePreview() {
     HistoricScreen(
         historicState = RecipeHistoricUiState.Success(listOf()),
+        navController = rememberNavController(),
     )
 }
