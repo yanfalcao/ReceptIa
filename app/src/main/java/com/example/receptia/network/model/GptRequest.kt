bd39c6756fea3277176cfefd7c7cdfd5d366a11d
yanfalcao <yanfalcao10@gmail.com>
2023-11-07 00:15:18 -0300

    Update ChatGPT model

--- app/src/main/java/com/example/receptia/network/model/GptRequest.kt
+++ app/src/main/java/com/example/receptia/network/model/GptRequest.kt
@@ -3,7 +3,7 @@ package com.example.receptia.network.model
 import com.google.gson.annotations.SerializedName
 
 data class GptRequest(
-    val model: String = "gpt-3.5-turbo-0613",
+    val model: String = "gpt-3.5-turbo-1106",
     val messages: List<GtpMessage>,
     val functions: List<GptFunctions>,
     @SerializedName("function_call")
