d27f65ec290164b8f9baae76a167a44636b0c1ef
yanfalcao <yanfalcao10@gmail.com>
2024-01-18 19:34:32 -0300

    Change SplashScreen.kt to dark and light mode

--- app/src/main/java/com/nexusfalcao/receptia/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/nexusfalcao/receptia/feature/splash/SplashScreen.kt
@@ -1,15 +1,16 @@
 package com.nexusfalcao.receptia.feature.splash
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -18,6 +19,9 @@ import androidx.navigation.NavController
 import com.nexusfalcao.receptia.R
 import com.nexusfalcao.receptia.feature.home.navigation.navigateToHome
 import com.nexusfalcao.receptia.feature.login.navigation.navigateToLogin
+import com.nexusfalcao.receptia.ui.preview.ThemePreviewShowsBakground
+import com.nexusfalcao.receptia.ui.theme.ReceptIaTheme
+import com.nexusfalcao.receptia.ui.theme.logoIconResource
 
 @Composable
 internal fun SplashRoute(
@@ -39,20 +43,26 @@ internal fun SplashRoute(
     SplashScreen()
 }
 
-@Preview(
-    showBackground = true,
-    showSystemUi = true,
-)
 @Composable
 fun SplashScreen() {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center,
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ic_logo),
+            painter = logoIconResource(),
             contentDescription = stringResource(id = R.string.logo_icon_description),
             Modifier.scale(1.1f),
         )
+    }
+}
+
+@ThemePreviewShowsBakground
+@Composable
+fun SplashScreenPreview() {
+    ReceptIaTheme {
+        SplashScreen()
     }
 }
