a9cbdc33859d1226ae2b644f9d1c9c3dd23b1df9
yanfalcao <yanfalcao10@gmail.com>
2024-02-26 23:30:53 -0300

    Implement init tests to RecipeDao

--- core/database/src/main/java/com/nexusfalcao/database/dao/IngredientDao.kt
+++ core/database/src/main/java/com/nexusfalcao/database/dao/IngredientDao.kt
@@ -3,10 +3,19 @@ package com.nexusfalcao.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
 import com.nexusfalcao.database.model.IngredientEntity
 
 @Dao
 interface IngredientDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(vararg ingredients: IngredientEntity)
+
+    @Transaction
+    @Query("SELECT * FROM ingredient")
+    fun findAll(): List<IngredientEntity>
+
+    @Query("DELETE FROM ingredient WHERE recipe_id = :recipeId")
+    fun deleteByRecipeId(recipeId: String): Int
 }
