247b1d32dba6f12ab3b3c4e3c32da5d8c229e1bb
yanfalcao <yanfalcao10@gmail.com>
2024-05-08 17:10:41 -0300

    Remove unused

--- app/src/main/java/com/nexusfalcao/receptia/feature/splash/SplashScreen.kt
+++ app/src/main/java/com/nexusfalcao/receptia/feature/splash/SplashScreen.kt
@@ -11,11 +11,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
-import com.nexusfalcao.receptia.R
 import com.nexusfalcao.home.navigation.navigateToHome
 import com.nexusfalcao.login.navigation.navigateToLogin
 import com.nexusfalcao.designsystem.preview.ThemePreviewShowsBakground
