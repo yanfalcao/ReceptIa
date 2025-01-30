c074e609aaf6a8b732b56c9b3f95f287512bf089
yanfalcao <yanfalcao10@gmail.com>
2025-01-30 15:44:35 -0300

    Adjust contentDescription on DrawerMenu

--- core/designsystem/src/main/java/com/nexusfalcao/designsystem/widget/navigationDrawer/DrawerHeader.kt
+++ core/designsystem/src/main/java/com/nexusfalcao/designsystem/widget/navigationDrawer/DrawerHeader.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.nexusfalcao.designsystem.R
@@ -30,7 +31,7 @@ internal fun DrawerHeader(
     ) {
         Image(
             painter = painterResource(id = avatarRes),
-            contentDescription = null,
+            contentDescription = stringResource(R.string.cd_avatar),
             modifier =
             Modifier
                 .height(85.dp)
